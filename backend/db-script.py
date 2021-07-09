9aa3560ba69e48f48df0843d2ca25be19999008a
Aaron <chanman2841@gmail.com>
2021-07-08 20:03:49 -0500

    setup query data method

--- backend/db-script.py
+++ backend/db-script.py
@@ -10,9 +10,9 @@ from secret import APP_TOKEN
 # startyear should be a string for the year we want to start the query 
 # endyear should be the string for the year we want to end the query  
 class CrimeData:
-    def __init__(self, params, startyear, endyear):
 
-        # turn params into comma seperated string
+    def __init__(self, params, startyear, endyear):
+        # turn params into comma seperated string attribute
         self.params = ''
         for i in range(0,len(params)):
             if i == len(params)-1:
@@ -21,14 +21,30 @@ class CrimeData:
             else:
                 self.params += params[i] + ','
             
+        # Starting year of data we want to query
         self.startyear = startyear
+        # Ending year of data we want to query
         self.endyear = endyear
+
+    def get_data(self,limit='10000'):
+        # String literal 
+        url = f'https://data.cityofchicago.org/resource/ijzp-q8t2.json?$select={self.params}&$where=year<={self.endyear} AND year >= {self.startyear}&$order=date ASC&$limit={limit}'
+        # Using our app token
+        headers = {'Accept': 'application/json', 'X-App-Token': APP_TOKEN}
+        resp = requests.get(url,headers=headers)
+        df = json.loads(resp.text)
+
+        # Return the queried data excluding na rows
+        self.data = pd.DataFrame(df).dropna()
+        return self.data
     
 def main():
-    test = CrimeData(['hi','test','again'],'2010','2018')
+    test = CrimeData(params=['date','community_area'],startyear='2004',endyear='2005')
+    test.get_data(limit='10000')
     print(test.params)
     print(test.startyear)
     print(test.endyear)
+    print(test.data)
 
 if __name__ == "__main__":
     main()
